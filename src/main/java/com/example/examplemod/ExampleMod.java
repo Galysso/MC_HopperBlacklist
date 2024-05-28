abe13892e3b4f5a50bbac9b1752ae43506300b61
Galysso <jojomessagerie@hotmail.fr>
2024-05-28 16:34:36 +0200

    Cleared project to start implementing blacklist feature

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -2,7 +2,6 @@ package com.example.examplemod;
 
 import net.minecraftforge.fml.common.Mod;
 
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
 public class ExampleMod
---
+++ src/main/java/com/example/examplemod/mixin/HopperBlacklistMixin.java
@@ -0,0 +1,8 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(HopperBlockEntity.class)
+public class HopperBlacklistMixin {
+}
