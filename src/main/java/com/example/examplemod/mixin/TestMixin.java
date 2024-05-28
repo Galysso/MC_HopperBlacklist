82f4b9ace7e783cb9126bf1448dcb47e67e67416
Galysso <jojomessagerie@hotmail.fr>
2024-05-28 12:26:33 +0200

    Cleared TestMixin.java for easier reading

--- src/main/java/com/example/examplemod/mixin/TestMixin.java
+++ src/main/java/com/example/examplemod/mixin/TestMixin.java
@@ -4,28 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BrewingStandBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(BrewingStandBlock.class)
 public class TestMixin {
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    static {
-        LOGGER.info("TestMixin class is being loaded");
-    }
-
     @Overwrite
     public boolean hasAnalogOutputSignal(BlockState state) {
-        LOGGER.info("Entering hasAnalogOutputSignal method");
         return false;
     }
 
     @Overwrite
     public int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos) {
-        LOGGER.info("Entering getAnalogOutputSignal method");
         return 0;
     }
 }
