794e4852754824b06ce549fb7748f774874b8cc8
Galysso <jojomessagerie@hotmail.fr>
2024-05-28 11:49:27 +0200

    Initial commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,13 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("minecraftmod")
+public class ExampleMod
+{
+    public ExampleMod() {
+        // nothing
+    }
+}
