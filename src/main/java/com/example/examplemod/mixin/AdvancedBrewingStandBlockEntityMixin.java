fb80b5d4973565c5fb105a2ad8eb919778880bf8
Galysso <jojomessagerie@hotmail.fr>
2024-05-28 21:22:56 +0200

    Trying to add a Dave's Potioneering as a dependency

--- src/main/java/com/example/examplemod/mixin/AdvancedBrewingStandBlockEntityMixin.java
+++ src/main/java/com/example/examplemod/mixin/AdvancedBrewingStandBlockEntityMixin.java
@@ -1,6 +1,10 @@
 package com.example.examplemod.mixin;
 
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import tfar.davespotioneering.blockentity.AdvancedBrewingStandBlockEntity;
 
 @Mixin(value = AdvancedBrewingStandBlockEntity.class, remap = false)
 public class AdvancedBrewingStandBlockEntityMixin {
