b72105eabd5b374ba626ca72f8d28a083e2b8058
Galysso <jojomessagerie@hotmail.fr>
2024-05-29 18:30:26 +0200

    Minecraft Brewing stand and Dave's Brewing Stand are correctly disabled, but Sinytra enables them back

---
+++ src/main/java/com/example/examplemod/mixin/BNBrewingStandBlockEntityMixin.java
@@ -0,0 +1,11 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.world.item.ItemStack;
+//import org.betterx.betternether.blockentities.BNBrewingStandBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+//@Mixin(value = BNBrewingStandBlockEntity.class, remap = false)
+public class BNBrewingStandBlockEntityMixin {
+
+}
--- src/main/java/com/example/examplemod/mixin/BrewingStandBlockEntityMixin.java
+++ src/main/java/com/example/examplemod/mixin/BrewingStandBlockEntityMixin.java
@@ -1,14 +1,38 @@
 package com.example.examplemod.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BrewingStandBlock;
+import net.minecraft.world.level.block.RedStoneOreBlock;
+import net.minecraft.world.level.block.RedstoneTorchBlock;
 import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.phys.shapes.Shapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BrewingStandBlockEntity.class)
 public abstract class BrewingStandBlockEntityMixin {
+
+
     @Overwrite
     public boolean canPlaceItem(int slot, ItemStack stack) {
         return false;
@@ -18,4 +42,14 @@ public abstract class BrewingStandBlockEntityMixin {
     public boolean canTakeItemThroughFace(int p_18952_, ItemStack p_18953_, Direction direction) {
         return false;
     }
+
+    /*@Overwrite
+    public int getAnalogOutputSignal(BlockState p_50926_, Level p_50927_, BlockPos p_50928_) {
+        return 1;
+    }
+
+    @Overwrite
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_50948_) {
+        p_50948_.add(new Property[]{HAS_BOTTLE[0], HAS_BOTTLE[1], HAS_BOTTLE[2]});
+    }*/
 }
