1eed8ae7a9424e96837cab3b0239ff275a8f64b8
Galysso <jojomessagerie@hotmail.fr>
2024-05-30 17:13:03 +0200

    Using hoppers to manage items inside the brewing stands is successfully disabled, except when using Sinytra Connector

--- src/main/java/com/example/examplemod/mixin/BrewingStandBlockEntityMixin.java
+++ src/main/java/com/example/examplemod/mixin/BrewingStandBlockEntityMixin.java
@@ -1,55 +1,29 @@
 package com.example.examplemod.mixin;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.Container;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BrewingStandBlock;
-import net.minecraft.world.level.block.RedStoneOreBlock;
-import net.minecraft.world.level.block.RedstoneTorchBlock;
 import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.phys.shapes.Shapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.Property;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BrewingStandBlockEntity.class)
+import javax.annotation.Nullable;
+
+@Mixin(value = BrewingStandBlockEntity.class)
 public abstract class BrewingStandBlockEntityMixin {
-
+    private int[] noSlotsForHopper = new int[0];
 
     @Overwrite
-    public boolean canPlaceItem(int slot, ItemStack stack) {
+    public int[] getSlotsForFace(Direction p_59010_) {
+        return this.noSlotsForHopper;
+    }
+
+    @Overwrite
+    public boolean canPlaceItemThroughFace(int var1, ItemStack var2, @Nullable Direction var3) {
         return false;
     }
 
     @Overwrite
-    public boolean canTakeItemThroughFace(int p_18952_, ItemStack p_18953_, Direction direction) {
+    public boolean canTakeItemThroughFace(int var1, ItemStack var2, Direction var3) {
         return false;
     }
-
-    /*@Overwrite
-    public int getAnalogOutputSignal(BlockState p_50926_, Level p_50927_, BlockPos p_50928_) {
-        return 1;
-    }
-
-    @Overwrite
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_50948_) {
-        p_50948_.add(new Property[]{HAS_BOTTLE[0], HAS_BOTTLE[1], HAS_BOTTLE[2]});
-    }*/
 }
---
+++ src/main/java/com/example/examplemod/mixin/DavesPotioneering_BrewingHandlerMixin.java
@@ -0,0 +1,16 @@
+package com.example.examplemod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import net.minecraft.core.Direction;
+import tfar.davespotioneering.inv.BrewingHandler;
+
+@Mixin(value = BrewingHandler.class, remap = false)
+public abstract class DavesPotioneering_BrewingHandlerMixin {
+    private int[] noSlotsForHopper = new int[0];
+
+    @Overwrite
+    public int[] getSlotsForFace(Direction side) {
+        return new int[0];
+    }
+}
